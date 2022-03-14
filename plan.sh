1f39bdc2d7eec79991e85e26318dead2d7c6b18f
RaviDuddela <RaviShankar.Duddela@progress.com>
2022-03-14 11:53:34 +0530

    buildissue as 3.8.4 is not available.upgrading to 3.8.5
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=maven
-pkg_version=3.8.4
+pkg_version=3.8.5
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A software project management and comprehension tool"
 pkg_upstream_url="https://maven.apache.org/"
 pkg_license=("Apache-2.0")
 pkg_source="http://apache.cs.utah.edu/maven/maven-3/${pkg_version}/source/apache-maven-${pkg_version}-src.tar.gz"
-pkg_shasum=298bfb6172c134655f4f5608342f9c7bc798918ac165b8ded4af800be184ab9b
+pkg_shasum=a60ddbb333f3addb93e063613a9589f47ed805a572c156075dc1bf4db774c007
 pkg_dirname="apache-${pkg_name}-${pkg_version}"
 pkg_deps=(
   core/coreutils
